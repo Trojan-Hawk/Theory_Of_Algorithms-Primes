f66b6d295b82eb83ffd558193a9d6528cbed1982
Tim <g00333333@gmit.ie>
2019-02-02 00:00:00 +0000

    Improved prime number function and added command line arguments to be input

--- primes.c
+++ primes.c
@@ -1,27 +1,38 @@
 #include <stdio.h>
 
-int isPrime(unsigned int test){
-	unsigned int i;
+int isPrime(unsigned int test) {
+    unsigned int i;
+    
+    // check to see if the value is prime
+    for(i = 2; i <= test / 2; ++i) {
+        if(test % i == 0) {
+	    return 0;
+        }// if
+    }// for
 
-  	for(i=2; i<test; i++){
-	 	 if(test % i == 0){
-			printf("%d is not prime!", test);
-			return 0;
-	 	 }
-	}
-	return 1;
-}
+    return 1;
+}// isPrime
 
 int main(int argc, char *argv[]) {
-
-  unsigned int test = 563;
-
-	if(isPrime(test)){
-		printf("Is Prime\n");
-	} else {
-		printf("Not Prime!\n");
-	}
-
-  return 0;
-}
+    
+    unsigned int argLength = argc, i, test;
+    
+    // iterate through each cmd line argument passed in
+    for(i = 1; i < argLength; i++) {
+        // convert the char array into an int value
+        sscanf(argv[i], "%d", &test);
+        
+        printf("%d cmd arg %d", test, i);
+        
+        // check to see if the value is prime
+        if(isPrime(test)) {
+            printf("\n%d is a Prime\n", test);
+        } else {
+            printf("\n%d is not a Prime!\n", test);
+        }// if/else   
+    
+    }// for
+   
+    return 0;
+}// main
 
